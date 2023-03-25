78a19ea718f47f086213ae6f0fd5f18b64433e5b
Amine derbal <aminedoran31@gmail.com>
2023-03-25 17:05:59 +0100

    add TodoList component

---
+++ src/components/TodosList.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const TodosList = ({ todosProps, handleChange, delTodo }) => (
+  <ul>
+    {todosProps.map((todo) => (
+      <TodoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo} />
+    ))}
+  </ul>
+);
+
+TodosList.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+  todosProps: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+export default TodosList;
