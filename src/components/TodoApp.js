f13eeb7aca6be77dd89d134a74b1abc74a777884
Amine derbal <aminedoran31@gmail.com>
2023-03-25 17:05:30 +0100

    add TodoApp component

---
+++ src/components/TodoApp.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+import TodosLogic from './TodosLogic';
+
+const TodoApp = () => (
+  <div>
+    <Header />
+    <TodosLogic />
+  </div>
+);
+
+export default TodoApp;
