ba6d76591f6b1db4ee7b4e73fd3e796dfabe62cc
Amine derbal <aminedoran31@gmail.com>
2023-03-26 16:14:24 +0100

    config sass

--- webpack.common.js
+++ webpack.common.js
@@ -22,8 +22,15 @@ module.exports = {
         use: [{ loader: 'babel-loader' }],
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
