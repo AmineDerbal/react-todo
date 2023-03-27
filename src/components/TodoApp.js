1653323e6ed3d0063efe563dc453ce91f6194179
Amine derbal <aminedoran31@gmail.com>
2023-03-27 09:32:45 +0100

    add routes

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,13 +1,33 @@
-import Header from './Header';
-import TodosLogic from './TodosLogic';
+import { Routes, Route } from 'react-router-dom';
+import SinglePage from '../routes/SinglePage';
+
+import Home from '../routes/Home';
+import About from '../routes/About';
+import Login from '../routes/Login';
+import Profile from '../routes/Profile';
+import NotMatch from '../routes/NotMatch';
+import Layout from './Layout';
+import ProtectedRoute from './ProtectedRoute';
 
 const TodoApp = () => (
-  <div className="wrapper">
-    <div className="todos">
-      <Header />
-      <TodosLogic />
-    </div>
-  </div>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />}>
+        <Route path=":slug" element={<SinglePage />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route
+        path="profile"
+        element={(
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        )}
+      />
+      <Route path="*" element={<NotMatch />} />
+    </Route>
+  </Routes>
 );
 
 export default TodoApp;
